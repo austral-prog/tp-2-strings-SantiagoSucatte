c34c7047d075bdb961afcb948b8443da8a5a20e3
SantiagoSucatte <ssucatte@mail.austral.edu.ar>
2026-04-06 10:19:31 -0300

    Delete tp-2-strings-SantiagoSucatte-main directory

--- tp-2-strings-SantiagoSucatte-main/casting.py
+++
@@ -1,9 +0,0 @@
-def casting():
-    """Lee precio, descuento y cantidad como texto y calcula el precio con descuento y el total."""
-
-    precio = int(input("Precio: "))
-    descuento = float(input("Descuento: "))
-    cantidad = int(input("Cantidad: "))
-    print(f"Precio: {precio}\nDescuento: {descuento}\nPrecio con descuento: {precio - descuento}")
-    total = (precio - descuento) * cantidad
-    print(f"Total: {total}")
--- tp-2-strings-SantiagoSucatte-main/in_string.py
+++
@@ -1,8 +0,0 @@
-def check_vowels():
-    """Lee un nombre y verifica si contiene cada una de las vocales (a, e, i, o, u),
-    sin distinguir mayúsculas de minúsculas.
-    """
-
-    nombre = input("Nombre: ").lower()
-    print(f"Contiene a: {"a" in nombre}\nContiene e: {"e" in nombre}\nContiene i: {"i" in nombre}\nContiene o: {"o" in nombre}\nContiene u: {"u" in nombre}")
-
