0e1fc3be3e469e4c6ca501549e0a73866e15b061
SantiagoSucatte <ssucatte@mail.austral.edu.ar>
2026-04-06 10:27:20 -0300

    Update ficha.py

--- ficha.py
+++ ficha.py
@@ -37,14 +37,14 @@ def ficha():
     print(f"Nombre: {nombre_limpio}")
     print(f"Email: {email.lower()}")
     print(f"Caracteres en nombre: {len(nombre_limpio)}")
-    print(f"Iniciales: {nombre_limpio[0] + nombre_limpio[nombre_limpio.find(" ") + 1]}")
-    nombre_solo = nombre_limpio[:nombre_limpio.find(" ")]
-    apellido = nombre_limpio[nombre_limpio.find(" ") + 1:]
+    print(f"Iniciales: {nombre_limpio[0] + nombre_limpio[nombre_limpio.find(' ') + 1]}")
+    nombre_solo = nombre_limpio[:nombre_limpio.find(' ')]
+    apellido = nombre_limpio[nombre_limpio.find(' ') + 1:]
     print(f"Usuario: {apellido.lower()}.{nombre_solo.lower()}")
     print(f"Email valido: {"@" in email}")
-    print(f"Dominio: {email.lower()[email.find("@") + 1:]}")
-    print(f"Nombre para archivo: {nombre_limpio.replace(" ", "_")}")
-    print(f"Cantidad de a: {nombre_limpio.lower().count("a")}")
+    print(f"Dominio: {email.lower()[email.find('@') + 1:]}")
+    print(f"Nombre para archivo: {nombre_limpio.replace(' ', '_')}")
+    print(f"Cantidad de a: {nombre_limpio.lower().count('a')}")
     print(f"Codigo secreto: {nombre_limpio[::-1].upper()}")
     n1 = int(nota1)
     n2 = int(nota2)
